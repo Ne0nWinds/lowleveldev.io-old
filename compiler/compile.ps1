50d163cae54d353814595634a24a034ebc7283ad
Caden Parker <cadenparker@ymail.com>
2021-11-07 15:15:57 -0800

    Link-time optimizations

--- compiler/compile.ps1
+++ compiler/compile.ps1
@@ -11,8 +11,8 @@ clang -O0 -D_DEBUG --target=wasm32 -msimd128 -nostdlib `
 -o binary.wasm `
 ../src/main.c ../src/tokenize.c ../src/standard_functions.c ../src/codegen.c
 } else {
-clang -Ofast --target=wasm32 -msimd128 -nostdlib `
-"-Wl,--no-entry,--allow-undefined-file=../imports.sym" `
+clang -Ofast -flto --target=wasm32 -msimd128 -nostdlib `
+"-Wl,--no-entry,--allow-undefined-file=../imports.sym,--lto-O3" `
 -Wno-incompatible-library-redeclaration -Wno-switch `
 -o binary.wasm `
 ../src/main.c ../src/tokenize.c ../src/standard_functions.c ../src/codegen.c
